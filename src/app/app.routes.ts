f8869c7508ef4d261a26dddfdd60215a8a81a868
KarinadelaRiva <164400941+KarinadelaRiva@users.noreply.github.com>
2025-11-22 22:47:57 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,16 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './guards/auth.guard';
+//import { authGuard } from './guards/auth.guard';
 import { Login } from './pages/login/login';
 import { Home } from './pages/home/home';
+import { Menu } from './pages/menu/menu';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/login',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    component: Login
-  },
-  {
-    path: 'home',
-    component: Home,
-    canActivate: [authGuard]
-  },
-  {
-    path: 'perfil',
-    loadComponent: () => import('./pages/perfil/perfil.component').then(m => m.PerfilComponent),
-    canActivate: [authGuard]
-  },
-    {path:'**', redirectTo:'home'}
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'login', component: Login },
+  { path: 'menu', component: Menu },
+  { path: 'perfil', component: PerfilComponent,/* canActivate: [authGuard]*/},
+  { path: '**', redirectTo: 'home' }
 ];
